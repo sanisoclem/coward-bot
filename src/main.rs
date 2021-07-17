9bd4eba61ad1d0a213fac1c77e0e690b8b4136f8
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2021-07-17 13:01:56 +1000

    idiot committed his api keys

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use coward_binance::*;
 use coward_exchange::*;
 fn main() {
   // get trading fees
-  let api_key = "9MvoV5LEbiRc6Kt045iHnwUbiwfved9pLHwblmPEB2qdUc0I905bGSY2wOCcS53P";
-  let api_secret = "51jUmQLfvzbUzEwCRY8AHqZyrKOcLZbVg5XL0MS88ZjVWDDInQj79sc41jMdIhlm";
+  let api_key = "";
+  let api_secret = "";
   let exchange = Binance::create(api_key, api_secret, BinanceEnvironment::Live);
 
   let test = exchange.get_trade_fees(&TradingPair::create(TickerSymbol::ETH, TickerSymbol::BTC));
